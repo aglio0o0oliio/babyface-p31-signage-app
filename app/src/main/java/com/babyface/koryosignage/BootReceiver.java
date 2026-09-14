3263ae1aba247addefb9bc8f48381b1f3680165e
aglio0o0oliio <triple_threater@yahoo.co.jp>
2026-09-14 10:53:13 +0900

    Create BootReceiver.java

---
+++ app/src/main/java/com/babyface/koryosignage/BootReceiver.java
@@ -0,0 +1,24 @@
+package com.babyface.koryosignage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())
+                || "android.intent.action.QUICKBOOT_POWERON".equals(intent.getAction())) {
+
+            Intent i = new Intent(context, MainActivity.class);
+
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                    | Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+            context.startActivity(i);
+        }
+    }
+}
